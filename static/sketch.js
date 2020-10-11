2f9417d17f42d5e224d5ad41285574a501370b04
thedoodler <ramprakashsmail@gmail.com>
2020-10-11 22:53:34 +0530

    added  mouse controls for fractal angle

--- static/sketch.js
+++ static/sketch.js
@@ -1,14 +1,28 @@
 
 function reportsize(){
-  init()
+  init();
 }
 
-window.addEventListener('resize',reportsize)
+window.addEventListener('resize',reportsize);
+
+function randcolor(){
+  var r = random(0,255);
+  var g = random(0,255);
+  var b = random(0,255);
+  console.log(r,g,b);
+  return color(r,g,b);
+}
+
+
+function fractal(start,end,ang){
+  
+}
 
 function init(){
   ww = window.innerWidth;
   wh = window.innerHeight;
   canvas = createCanvas(ww,wh);
+  background('black');
   canvas.position(0,0);
   canvas.style('z-index','-1');
 }
@@ -19,5 +33,5 @@ function setup(){
 
 
 function draw(){
-  circle(ww/2,wh/2,100);
+
 }
