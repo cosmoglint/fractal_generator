cacccd719d4a72052e76028b11ad540a9016e28f
thedoodler <ramprakashsmail@gmail.com>
2020-10-11 13:36:56 +0530

    minor changes

--- tests/fractal.js
+++ tests/fractal.js
@@ -1,25 +1,28 @@
 let slid;
-var a  = 0;
+
 var i = 0;
 function setup() {
   createCanvas(400,400);
-  
-  slid = createSlider(0,2 * PI,100,0.01);
-  
-  
+  var p  = PI;
+  var angle = PI/4;
+
+  slid = createSlider(0,4*p ,100,0.01);
+
+
 }
 
 function draw() {
   background(200);
   var n = height*0.4;
-  translate(width/2,height);
-  a = a+((2*PI)/(360-i));
-  if (i==360){
-    i = 0;
-    a = 0;
-  }
-  console.log(((2*PI)/(360-i)));
-  i = i + 1;
+  translate(width/2,height); //move the origin to root of the tree
+  // a = ((2*PI)/(360-i));
+  a = slid.value();
+  // if (i==360){
+  //   i = 0;
+  //   a = 0;
+  // }
+  // console.log(((4*PI)/(360-i)));
+  // i = i + 1;
   recu(n);
 
 
@@ -28,7 +31,7 @@ function draw() {
 function recu(n){
   line(0,0,0,-n);
   translate(0,-n);
-  if (n>25) {
+  if (n>10) {
     push();
     rotate(a);
     recu(n*0.6);
